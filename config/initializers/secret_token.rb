08b7338daec589269966ff6ae0a456e001911dea
MPS <genni951@gmail.com>
2010-11-14 17:45:52 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballStats::Application.config.secret_token = '1ed10493204f000a76892bfc162b98ca517fda36abd9521d7710beb964eac6d277642641216334c5967e3f73c91ab0a4e392d7bf0a91ba8443a994306eec4f45'
